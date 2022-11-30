0757036b5ccda4daf9aca306153af4d39dbe0ca9
naila <naila.pineda09@gmail.com>
2022-11-30 03:51:51 +0000

    load in data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
+import ColorPalette from "./components/ColorPalette";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ColorPalette />
     </div>
   );
 }
